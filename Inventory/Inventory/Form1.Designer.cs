e3d51ed124e483d8b0bee872842589adc0acac25
DESKTOP-5QDPS6T\dell <patrickgonzales.ojt1710152@gmail.com>
2018-02-14 07:56:18 -0800

    Added Inventory file project

---
+++ Inventory/Inventory/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Inventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkFullSet = new System.Windows.Forms.CheckBox();
+            this.chkMeat = new System.Windows.Forms.CheckBox();
+            this.chkRice = new System.Windows.Forms.CheckBox();
+            this.chkBeans = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstBrand = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.txt_CustomerID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbOrderMade = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(31, 223);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(91, 24);
+            this.btnOrder.TabIndex = 0;
+            this.btnOrder.Text = "Order ";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.BtnOrder);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(387, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 209);
+            this.panel1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Controls.Add(this.chkFullSet);
+            this.groupBox2.Controls.Add(this.chkMeat);
+            this.groupBox2.Controls.Add(this.chkRice);
+            this.groupBox2.Controls.Add(this.chkBeans);
+            this.groupBox2.Location = new System.Drawing.Point(228, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(136, 150);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Items";
+            // 
+            // chkFullSet
+            // 
+            this.chkFullSet.AutoSize = true;
+            this.chkFullSet.Location = new System.Drawing.Point(15, 108);
+            this.chkFullSet.Name = "chkFullSet";
+            this.chkFullSet.Size = new System.Drawing.Size(61, 17);
+            this.chkFullSet.TabIndex = 5;
+            this.chkFullSet.Text = "Full Set";
+            this.chkFullSet.UseVisualStyleBackColor = true;
+            this.chkFullSet.CheckedChanged += new System.EventHandler(this.chkFullSet_CheckedChanged);
+            // 
+            // chkMeat
+            // 
+            this.chkMeat.AutoSize = true;
+            this.chkMeat.Location = new System.Drawing.Point(15, 82);
+            this.chkMeat.Name = "chkMeat";
+            this.chkMeat.Size = new System.Drawing.Size(50, 17);
+            this.chkMeat.TabIndex = 4;
+            this.chkMeat.Text = "Meat";
+            this.chkMeat.UseVisualStyleBackColor = true;
+            // 
+            // chkRice
+            // 
+            this.chkRice.AutoSize = true;
+            this.chkRice.Location = new System.Drawing.Point(15, 50);
+            this.chkRice.Name = "chkRice";
+            this.chkRice.Size = new System.Drawing.Size(48, 17);
+            this.chkRice.TabIndex = 3;
+            this.chkRice.Text = "Rice";
+            this.chkRice.UseVisualStyleBackColor = true;
+            this.chkRice.CheckedChanged += new System.EventHandler(this.chkRice_CheckedChanged);
+            // 
+            // chkBeans
+            // 
+            this.chkBeans.AutoSize = true;
+            this.chkBeans.Location = new System.Drawing.Point(15, 26);
+            this.chkBeans.Name = "chkBeans";
+            this.chkBeans.Size = new System.Drawing.Size(56, 17);
+            this.chkBeans.TabIndex = 2;
+            this.chkBeans.Text = "Beans";
+            this.chkBeans.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox3.Controls.Add(this.rb4);
+            this.groupBox3.Controls.Add(this.rb3);
+            this.groupBox3.Controls.Add(this.rb2);
+            this.groupBox3.Controls.Add(this.rb1);
+            this.groupBox3.Location = new System.Drawing.Point(411, 31);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(136, 150);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Quantity";
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(21, 105);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(31, 17);
+            this.rb4.TabIndex = 9;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "4";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(21, 82);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(31, 17);
+            this.rb3.TabIndex = 8;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "3";
+            this.rb3.UseVisualStyleBackColor = true;
+            this.rb3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(21, 49);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(31, 17);
+            this.rb2.TabIndex = 7;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "2";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(21, 26);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(31, 17);
+            this.rb1.TabIndex = 6;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "1";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.lstBrand);
+            this.groupBox1.Location = new System.Drawing.Point(30, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 150);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Brands";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lstBrand
+            // 
+            this.lstBrand.FormattingEnabled = true;
+            this.lstBrand.Location = new System.Drawing.Point(6, 26);
+            this.lstBrand.Name = "lstBrand";
+            this.lstBrand.Size = new System.Drawing.Size(115, 108);
+            this.lstBrand.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Controls.Add(this.lstOutput);
+            this.panel2.Location = new System.Drawing.Point(55, 286);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(906, 209);
+            this.panel2.TabIndex = 5;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(3, 12);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(889, 186);
+            this.lstOutput.TabIndex = 0;
+            // 
+            // txt_CustomerID
+            // 
+            this.txt_CustomerID.Location = new System.Drawing.Point(129, 36);
+            this.txt_CustomerID.Name = "txt_CustomerID";
+            this.txt_CustomerID.Size = new System.Drawing.Size(100, 20);
+            this.txt_CustomerID.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Date Ordered";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Order";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Name";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(129, 158);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // cmbOrderMade
+            // 
+            this.cmbOrderMade.FormattingEnabled = true;
+            this.cmbOrderMade.Location = new System.Drawing.Point(129, 115);
+            this.cmbOrderMade.Name = "cmbOrderMade";
+            this.cmbOrderMade.Size = new System.Drawing.Size(121, 21);
+            this.cmbOrderMade.TabIndex = 20;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(129, 75);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Customer ID";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(187, 223);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(91, 24);
+            this.btnExit.TabIndex = 21;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 541);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cmbOrderMade);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txt_CustomerID);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkFullSet;
+        private System.Windows.Forms.CheckBox chkMeat;
+        private System.Windows.Forms.CheckBox chkRice;
+        private System.Windows.Forms.CheckBox chkBeans;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstBrand;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.TextBox txt_CustomerID;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbOrderMade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Inventory/Inventory/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class Form1 : Form
+    {
+        String OrderDetails = "{0, -50}, {1, -50}, {2, -50}, {3, -50}, {4, -50}";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button Clicked!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+                lstOutput.Items.Add(String.Format(OrderDetails, "Customer ID", "Name", "Order Made", "Date Ordered", "Price"));
+            cmbOrderMade.Items.Add("Select Order");
+            cmbOrderMade.Items.Add("Account");
+            cmbOrderMade.Items.Add("Telephone");
+            cmbOrderMade.Items.Add("Instore Order");
+            cmbOrderMade.Items.Add("Direct Debit");
+
+            lstBrand.Items.Add("Japan");
+            lstBrand.Items.Add("Tokyo");
+            lstBrand.Items.Add("Chigoya");
+            lstBrand.Items.Add("Nagoya");
+
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOrder(object sender, EventArgs e)
+        {
+            String ID, name, order, date, price;
+            ID = txt_CustomerID.Text;
+            name = txtName.Text;
+            order = cmbOrderMade.Text;
+            date = dateTimePicker1.Text;
+
+            if (lstBrand.Text == "")
+            {
+                MessageBox.Show("You must select a brand", "Brands", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if ((chkBeans.Checked == false) && (chkMeat.Checked == false) && (chkRice.Checked == false)) {
+                MessageBox.Show("You must select an item", "Items", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if ((rb1.Checked == false) && (rb2.Checked == false) && (rb3.Checked == false))
+            {
+                MessageBox.Show("You must select an quantity", "Quantity", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkRice_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult Iexit = MessageBox.Show("Exit the App?", "Inventory System", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (Iexit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void chkFullSet_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkFullSet.Checked)
+            {
+                chkBeans.Checked = true;
+                chkRice.Checked = true;
+                chkMeat.Checked = true;
+            }
+            else {
+                chkBeans.Checked = false;
+                chkRice.Checked = false;
+                chkMeat.Checked = false;
+            }
+        }
+    }
+}
